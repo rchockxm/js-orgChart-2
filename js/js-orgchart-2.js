863db8d0814bbaa05c18a0f44108e0cb10dcb5d5
Rchockxm <rchockxm.silver@gmail.com>
2015-03-08 19:39:40 +0800

    Update js-orgchart-2.js

--- js/js-orgchart-2.js
+++ js/js-orgchart-2.js
@@ -1,1 +1,7 @@
-
+/*
+ * js-orgChart - 2.0
+ * Copyright (c) 2015 rchockxm (rchockxm.silver@gmail.com)
+ *
+ * Licensed - under the terms of the MIT License
+ * http://www.opensource.org/licenses/mit-license.php
+ */
